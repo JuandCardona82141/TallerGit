e756d56d140fdf8c06a5b29ff3c7d299da69efe4
JuanCardona <juandavicard2008@gmail.com>
2020-07-10 15:47:43 -0500

    Firts commit

---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal
+{
+    public static void main (String[] args)
+    {
+        System.out.println("\nÉste es un taller de git, prueba cambio dentro del código " +
+                "Rama Master");
+    }
+}
