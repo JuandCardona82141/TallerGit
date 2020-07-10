65f81ac680b3b86f5972b2187b60a9c685320a4c
JuanCardona <juandavicard2008@gmail.com>
2020-07-10 17:33:37 -0500

    Cambio master

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,6 @@ public class Principal
 {
     public static void main (String[] args)
     {
-        System.out.println("\nÉste es un taller de git, prueba cambio dentro del código " +
-                "Rama Master");
+        System.out.println("\ncambio");
     }
 }
